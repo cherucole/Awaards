639ac692ed4a942edae30a8e7c0c09613a12ffe7
cherucole <cheruyotcheruyot@gmail.com>
2018-10-15 11:25:50 +0300

    added tests

--- app/tests.py
+++ app/tests.py
@@ -4,10 +4,9 @@ from .models import *
 # Create your tests here.
 
 class ProfileTest(TestCase):
-
     def setUp(self):
         self.user = User.objects.create(id = 1, username='cheru')
-        self.profile = Profile.objects.create(user = self.user,bio = 'blow away')
+        self.profile = Profile.objects.create(user = self.user,bio = 'my sample bio', contact='mycontact')
 
     def test_instance(self):
         self.assertTrue(isinstance(self.profile,Profile))
@@ -25,38 +24,42 @@ class ProfileTest(TestCase):
         profile = Profile.find_profile('cheru')
         self.assertTrue(len(profile) > 0)
 
-class ImageTest(TestCase):
-
+class PostTest(TestCase):
     def setUp(self):
         self.user = User.objects.create(id = 1, username='cheru')
-        self.profile = Profile.objects.create(user = self.user,bio = 'my sample bio')
+        self.profile = Profile.objects.create(user = self.user,bio = 'my sample bio',contact='mycontact')
 
-        self.image = Post.objects.create(user_profile = self.user,
-                                          profile = self.profile,
-                                          caption ='this is it!',
-                                          likes = 0)
+        self.post= Post.objects.create(name='sample_post',
+                                       image='images/', description='desc',
+                                       live_link='http://url' )
+
 
     def test_instance(self):
-        self.assertTrue(isinstance(self.image,Post))
+        self.assertTrue(isinstance(self.post,Post))
 
     def test_all_posts(self):
-        self.image.save()
-        image = Post.all_posts()
-        self.assertTrue(len(image) > 0)
+        self.post.save()
+        post = Post.all_posts()
+        self.assertTrue(len(post) > 0)
 
-class CommentTest(TestCase):
-    def setUp(self):
-        self.user = User.objects.create(id = 1, username='cheru')
-
-        self.comment= Comment.objects.create(poster= self.user, comment='new comment' )
-
-    def test_instance(self):
-        self.assertTrue(isinstance(self.comment, Comment))
-
-    def test_save_comment(self):
-        self.assertTrue(isinstance(self.comment,Comment))
-
-    def test_get_all_comments(self):
-        self.comment.save()
-        comment = Comment.get_all_comments()
-        self.assertTrue(len(comment) > 0)
+    def test_search_post(self):
+        self.post.save()
+        post = Post.search_by_name('sample_post')
+        self.assertTrue(len(post) > 0)
+#
+# class PostTest(TestCase):
+#     def setUp(self):
+#         self.user = User.objects.create(id = 1, username='cheru')
+#
+#         self.post= Post.objects.create(poster= self.user, name='sample post', image='images/', description='desc', live_link='http://url' )
+#
+#     def test_instance(self):
+#         self.assertTrue(isinstance(self.post, Post))
+#
+#     def test_save_post(self):
+#         self.assertTrue(isinstance(self.post,Post))
+#
+#     def test_all_posts(self):
+#         self.post.save()
+#         post = Post.all_posts()
+#         self.assertTrue(len(post) > 0)
